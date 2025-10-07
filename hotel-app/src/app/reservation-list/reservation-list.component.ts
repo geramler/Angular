5d36412eb432e9cbfb81f85ff44de9799286647b
gerardo-ramirez <gerardo.ramirez@xa.epicgames.com>
2025-10-06 20:48:09 -0600

    Add hotel-app

---
+++ hotel-app/src/app/reservation-list/reservation-list.component.ts
@@ -0,0 +1,24 @@
+import { Component, OnInit } from '@angular/core';
+import { ReservationService } from '../reservation/reservation.service';
+import { Reservation } from '../models/reservation';
+
+@Component({
+  selector: 'app-reservation-list',
+  templateUrl: './reservation-list.component.html',
+  styleUrls: ['./reservation-list.component.css']
+})
+export class ReservationListComponent implements OnInit {
+  
+  reservations: Reservation[] = [];
+
+  constructor(private ReservationService : ReservationService) {}
+
+  ngOnInit(): void {
+    this.reservations = this.ReservationService.getReservations();
+  }
+
+  deleteReservation(id: string) {
+    this.ReservationService.deleteReservation(id);
+  }
+
+}
